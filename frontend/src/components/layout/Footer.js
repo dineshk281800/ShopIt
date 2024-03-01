62ee380bc8c9d49b2812b9be56cd835c5b41615b
Arunachalam13 <129979857+dineshk281800@users.noreply.github.com>
2024-03-01 11:22:25 +0530

    Update Footer.js

--- frontend/src/components/layout/Footer.js
+++ frontend/src/components/layout/Footer.js
@@ -4,7 +4,7 @@ const footer = () => {
   return (
     <footer class="py-1 pt-5">
       <p class="text-center mt-1 fw-bold">
-        ShopIT - 2020-2023, All Rights Reserved
+        ShopIT - Dinesh, All Rights Reserved
       </p>
     </footer>
   )
